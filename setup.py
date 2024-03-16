78d76933fe42fd4254ab7b37422a7b5c4b250aef
geohang <hangchen@u.boisestate.edu>
2024-03-15 18:15:36 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyCZINV',
-    version='0.1.0',
+    version='0.1.1',
     author='Hang Chen',
     author_email='hchen8@lbl.gov',
     packages=find_packages(),
